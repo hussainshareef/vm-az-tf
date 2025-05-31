c705fde1dfad764c4073e7b7da5d8f7bbc28bd2c
hussainshareef <mohammedhussainshareef68@gmail.com>
2025-05-31 22:07:34 +0530

    change in backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-backend-rg"
-    storage_account_name = "tfstate12345"
-    container_name       = "tfstate"
+    resource_group_name  = "Github-Selfhost-agent"
+    storage_account_name = "backendterraformsf"
+    container_name       = "backend"
     key                  = "my-infrastructure/terraform.tfstate"
   }
 }
