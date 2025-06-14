ff377ab98278c5bc488206c38671107537691982
Mohammed Hussain Shareef <114469176+hussainshareef@users.noreply.github.com>
2025-06-15 04:16:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "main" {
 
 resource "azurerm_public_ip" "vm" {
   name                = "${var.vm_config.name}-pip"
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Dynamic"
   tags                = var.vm_config.tags
 }
