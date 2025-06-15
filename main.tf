b2bb7c4eb41665aefea5a1446a9a06b8cac48ffd
Mohammed Hussain Shareef <114469176+hussainshareef@users.noreply.github.com>
2025-06-15 08:48:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,6 +26,20 @@ resource "azurerm_network_interface" "vm" {
   tags = var.vm_config.tags
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.vm_config.name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.vm_config.name}-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
 resource "azurerm_windows_virtual_machine" "vm" {
   count = var.vm_config.os_type == "windows" ? 1 : 0
 
